a5b7520aecee2cdeb74b2db9511d96f7839d9430
chrissyll <41743800+chrissyll@users.noreply.github.com>
2018-10-14 09:51:09 +0800

    ICON PACK NUGET INSTALLED

--- AirNewUI/LogIn.xaml.cs
+++ AirNewUI/LogIn.xaml.cs
@@ -29,40 +29,40 @@ namespace AirNewUI
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            bool loginSuccess = false;
-            while (!loginSuccess)
-            {
-                LoginDialogData result = await this.ShowLoginAsync("Login", "Enter your password",
-                   new LoginDialogSettings
-                   {
-                       ColorScheme = this.MetroDialogOptions.ColorScheme,
-                       RememberCheckBoxVisibility = Visibility.Visible,
-                       EnablePasswordPreview = true,
-                       //UsernameWatermark = "aaa"
-                   });
+            //bool loginSuccess = false;
+            //while (!loginSuccess)
+            //{
+            //    LoginDialogData result = await this.ShowLoginAsync("Login", "Enter your password",
+            //       new LoginDialogSettings
+            //       {
+            //           ColorScheme = this.MetroDialogOptions.ColorScheme,
+            //           RememberCheckBoxVisibility = Visibility.Visible,
+            //           EnablePasswordPreview = true,
+            //           //UsernameWatermark = "aaa"
+            //       });
 
-                if (result == null)
-                {
-                    MessageBox.Show("請輸入帳號密碼");
-                    continue;
-                }
-                else
-                {
-                    var res = dbContext.Members.AsEnumerable().FirstOrDefault(x =>
-                                x.Member_Account == result.Username && 
-                                x.Member_Password == result.Password);
+            //    if (result == null)
+            //    {
+            //        MessageBox.Show("請輸入帳號密碼");
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        var res = dbContext.Members.AsEnumerable().FirstOrDefault(x =>
+            //                    x.Member_Account == result.Username && 
+            //                    x.Member_Password == result.Password);
 
-                    if (res != null)
-                    {
-                        MessageBox.Show("登入成功");
-                        loginSuccess = true;
-                    }
-                    else
-                    {
-                        MessageBox.Show("帳號或密碼不正確");
-                    }
-                }
-            }
+            //        if (res != null)
+            //        {
+            //            MessageBox.Show("登入成功");
+            //            loginSuccess = true;
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("帳號或密碼不正確");
+            //        }
+            //    }
+            //}
         }
 
     }
