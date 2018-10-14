42b3e5758046d423f86ff18efe00f0a800a4cb46
chrissyll <41743800+chrissyll@users.noreply.github.com>
2018-10-14 09:43:48 +0800

    MVVM TEST

---
+++ AirNewUI/Lab/SampleData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirNewUI.Lab
+{
+    static class SampleData
+    {
+        public static List<Student> Students { get; set; }
+
+        public static void Seed()
+        {
+            Students = new List<Student>()
+            {
+                new Student { Id = 1, Name = "John", Age = 24, HasMarried = true },
+                new Student { Id = 2, Name = "Owen", Age = 23, HasMarried = false },
+                new Student { Id = 3, Name = "Joanna", Age = 32, HasMarried = true },
+                new Student { Id = 4, Name = "Catherine", Age = 38, HasMarried = true },
+                new Student { Id = 5, Name = "Tommy", Age = 21, HasMarried = false },
+                new Student { Id = 6, Name = "Leo", Age = 31, HasMarried = false },
+                new Student { Id = 7, Name = "Amy", Age = 26, HasMarried = false },
+                new Student { Id = 8, Name = "Emily", Age = 28, HasMarried = true }
+            };
+        }
+    }
+}
---
+++ AirNewUI/Lab/StudentViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AirNewUI.Lab
+{
+    public class StudentViewModel : INotifyPropertyChanged
+    {
+        public List<Student> Students { get; set; }
+        public Student Stud;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 建立Command物件，可設定於Button Command屬性中
+        /// </summary>
+        private ICommand command1;
+        public ICommand Command1
+        {
+            get { return this.command1 ?? (command1 = new RelayCommand { CanExecuteDelegate = x => true, ExecuteDelegate = x => this.PrefixNameWithAA() }); }
+        }
+        public StudentViewModel()
+        {
+            SampleData.Seed();
+            Students = SampleData.Students;
+            Stud = Students.First();
+
+            //Command1 = new RelayCommand
+            //{
+            //    CanExecuteDelegate = x => true,
+            //    ExecuteDelegate = x => this.PrefixNameWithAA()
+            //};
+        }
+
+        public string Name
+        {
+            get { return Stud.Name; }
+
+            set
+            {
+                if (Stud.Name != value)
+                {
+                    Stud.Name = value;
+                    //當值有改變時，觸發event
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        int count = 0;
+        public void PrefixNameWithAA()
+        {
+            ++count;
+            Name = "AA" + count;
+            command1.CanExecute(false);
+        }
+    }
+}
